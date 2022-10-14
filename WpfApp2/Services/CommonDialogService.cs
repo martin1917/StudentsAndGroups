fe9f415c340380d580f25a929a9a39d4ba615153
martin1917 <martin.levin02@mail.ru>
2022-10-14 19:58:24 +0300

    Сервис, который информирует юзера или запрашивает подтверждение для чего-то

---
+++ WpfApp2/Services/CommonDialogService.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace WpfApp2.Services;
+
+public class CommonDialogService
+{
+    public bool ConfirmInformation(string information, string caption)
+    {
+        return MessageBox.Show(information, caption, MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes;
+    }
+
+    public bool ConfirmWarning(string warning, string caption)
+    {
+        return MessageBox.Show(warning, caption, MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes;
+    }
+}
