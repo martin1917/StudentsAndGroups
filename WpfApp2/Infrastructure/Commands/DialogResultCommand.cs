1251916241f072a01f3d6dcd1563b45ab302b92e
martin1917 <martin.levin02@mail.ru>
2022-10-14 20:00:18 +0300

    Команда, которая устанавливает результат диалога для окна

---
+++ WpfApp2/Infrastructure/Commands/DialogResultCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp2.Infrastructure.Commands;
+
+public class DialogResultCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    public bool? DialogResult { get; set; }
+
+    public bool CanExecute(object? parameter) => App.CurrentWindow != null;
+
+    public void Execute(object? parameter)
+    {
+        if (!CanExecute(parameter)) return;
+
+        var window = App.CurrentWindow;
+
+        var dialog_result = DialogResult;
+        if (parameter != null)
+            dialog_result = Convert.ToBoolean(parameter);
+
+        window.DialogResult = dialog_result;
+        window.Close();
+    }
+}
